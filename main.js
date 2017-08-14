f45c5cc697fa8b6ed0adfe4a7a989bcb1d78566d
Shon Diaz <shondiaz@gmail.com>
2017-08-14 02:46:57 +0000

    annotations and prevent main.js from being called twice

--- main.js
+++ main.js
@@ -18,6 +18,10 @@ function createParagraph() {
 
 var buttons = document.querySelectorAll("button");
 
+// note: there is no html element that meets the selector "button"
+// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+console.log("test:", buttons);
+
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", createParagraph);
 }
