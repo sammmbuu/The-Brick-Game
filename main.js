b25a052d6ab27afda318506686fdd6a9df318998
Sam Hearn <samhearn2010@gmail.com>
2017-10-12 22:00:36 +0000

    10/12/17_1

--- main.js
+++ main.js
@@ -21,3 +21,7 @@ var buttons = document.querySelectorAll("button");
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", createParagraph);
 }
+
+function rect() {
+   
+}
